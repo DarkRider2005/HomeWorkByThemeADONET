bb12938cff9c1931409b8276ef1805aef9ae850d
Georgiy <98572930+DarkRider2005@users.noreply.github.com>
2024-03-04 19:24:38 +0500

    Add files via upload

---
+++ ДЗ от 28.02.2024/ViewModelCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public sealed class ViewModelCommand : ModelCommand
+    {
+        private Func<bool> _canExecute;
+        private Action _execute;
+
+        public ViewModelCommand(Func<bool> canExecute, Action execute)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        protected override bool CanExecute()
+        {
+            return _canExecute.Invoke();
+        }
+
+        protected override void Execute()
+        {
+            if (_execute != null)
+                _execute.Invoke();
+        }
+    }
+}
---
+++ ДЗ от 28.02.2024/ViewModelTableFromServer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class ViewModelTableFromServer
+    {
+        private string _id;
+        private string _title;
+        private string _type;
+        private string _color;
+        private string _calorie;
+
+        public string Id => _id;
+        public string Title => _title;
+        public string Type => _type;
+        public string Color => _color;
+        public string Calorie => _calorie;
+
+        public ViewModelTableFromServer(string id, string title, string type, string color, string calorie)
+        {
+            _id = id;
+            _title = title;
+            _type = type;
+            _color = color;
+            _calorie = calorie;
+        }
+    }
+}
